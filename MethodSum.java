7ec224b4416c1d297c6da02a84f78919259d017a
Dharanesh27 <dharaneshnsds@gmail.com>
2026-06-01 19:05:33 +0530

    Methods in java

---
+++ MethodSum.java
@@ -0,0 +1,9 @@
+class MethodSum{
+    public static void main(String[] args) {
+        System.out.println(sum(10,20));
+        
+    }
+    public static int sum(int a,int b){
+        return a + b;
+    }
+}
