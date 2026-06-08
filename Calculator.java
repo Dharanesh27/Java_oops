63a43fb2e3a082ec8b808dc3c0286c4c4990f1b3
Dharanesh27 <dharaneshnsds@gmail.com>
2026-06-08 09:24:17 +0530

    OOPS

--- Calculator.java
+++ Calculator.java
@@ -5,8 +5,14 @@ class Operations{
         this.num1 =num1;
         this.num2 =num2;
     }
-    void add(){
-        System.out.println("Added value: " + (num1+num2));
+    void add(int a,int b){
+        System.out.println("Added value: " + (a+b));
+    }
+    void add(double a,double b){
+        System.out.printf("%.2f%n", a + b);
+    }
+    void add(int a,int b,int c){
+        System.out.println(a+b+c);
     }
     void sub(){
         System.out.println("Subtracted value: "+ (num1-num2));
@@ -15,7 +21,9 @@ class Operations{
 class Calculator{
     public static void main(String[] args) {
         Operations obj = new Operations(10,20);
-        obj.add();
+        obj.add(10,20);
+        obj.add(11.0,22.0);
+        obj.add(10,10,20);
         obj.sub();
     }
 }
